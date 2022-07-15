deb1813a727b916ca8ad6ed259824a7ae1e1744f
xueyong <1085523029@qq.com>
2022-07-15 10:10:20 +0800

    :bug: PreviewClose icon color

--- components/icons.tsx
+++ components/icons.tsx
@@ -59,7 +59,13 @@ export function IconParkPreviewCloseOne(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 48 48"
       {...props}
     >
-      <g fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4">
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="4"
+      >
         <path d="M9.85786 18C6.23858 21 4 24 4 24C4 24 12.9543 36 24 36C25.3699 36 26.7076 35.8154 28 35.4921M20.0318 12.5C21.3144 12.1816 22.6414 12 24 12C35.0457 12 44 24 44 24C44 24 41.7614 27 38.1421 30"></path>
         <path d="M20.3142 20.6211C19.4981 21.5109 19 22.6972 19 23.9998C19 26.7612 21.2386 28.9998 24 28.9998C25.3627 28.9998 26.5981 28.4546 27.5 27.5705"></path>
         <path d="M42 42L6 6"></path>
